d5dc80432bc9b7f9e6205fdfc65e61b1e73f639b
jaizo <jaizon2000@gmail.com>
2021-03-22 16:05:27 -0600

    Create starter for ploty dash with dropdown

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 # Import packages for data cleaning
 import numpy as np
 import pandas as pd
-import re  # For finding specific strings in the text
 
+# IMport Ploty Dash
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
 
+# Create the app
+app = dash.Dash()
+
+# Add content
+app.layout = html.Div([
+    html.Label('Select a city:'),
+    dcc.Dropdown(
+        options=[
+            {'label': 'Edmonton', 'value': 'YEG'},
+            {'label': '', 'value': ''}
+        ],
+        value='YEG'
+    )
+])
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server()
