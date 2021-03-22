70db79d2e71d5a1eb8f61dbaabdd833c03205d7d
jaizo <jaizon2000@gmail.com>
2021-03-22 17:54:38 -0600

    Set up simple 3 column layout with dropdown

--- main.py
+++ main.py
@@ -2,29 +2,65 @@
 import numpy as np
 import pandas as pd
 
-# IMport Ploty Dash
+# Import Ploty Dash
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+
+# Bootstrap for Dash: https://bit.ly/3tKt9S3
+import dash_bootstrap_components as dbc
+
+# -------------------------------------------
 
 # Get data
 df = pd.read_csv('cmpt-courses-cleaned.csv')
 
 # Create the app
-app = dash.Dash()
+app = dash.Dash(
+    external_stylesheets=[dbc.themes.BOOTSTRAP]
+)
 
 # Add content
-app.layout = html.Div([
-    html.Label('Select a city:'),
-    dcc.Dropdown(
-        options=[
-            {'label': 'Edmonton', 'value': 'YEG'},
-            {'label': '', 'value': ''}
-        ],
-        value='YEG'
-    )
-])
+app.layout = html.Div(
+    [
+        dbc.Row(
+            [
+                # Column 1 - Input
+                dbc.Col(
+                    html.Div(
+                        [
+                            html.H1("Inputs"),  # Title
+                            dcc.Dropdown(
+                                options=[
+                                    {'label': 'Edmonton', 'value': 'YEG'},
+                                    {'label': '', 'value': ''}
+                                ],
+                                value='YEG'
+                            )
+                        ]
+                    ),
+                    width={"size": 3},
+                ),
+
+                # Column 2 - Data
+                dbc.Col(
+                    html.Div('Data'),
+                    width={"size": 3},
+
+                ),
+
+                # Column 3 - Checklist
+                dbc.Col(
+                    html.Div('Checklist'),
+                    width={"size": 3},
+
+                ),
+
+            ]
+        ),
+    ]
+)
 
 # Run the app
-# if __name__ == '__main__':
-#     app.run_server()
+if __name__ == '__main__':
+    app.run_server()
