2047d6545092e91316b9d152353c7b9490f9618f
jaizo <jaizon2000@gmail.com>
2021-03-26 17:54:06 -0600

    update layout of inputs. Set up multiple collapse bug: collapse and default/sunburst view btns give error when both active

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ import dash
 import dash_html_components as html
 import dash_core_components as dcc
 
+# App Callback for Dash
+from dash.dependencies import Input, Output, State
+
 # Bootstrap for Dash: https://bit.ly/3tKt9S3
 import dash_bootstrap_components as dbc
+
+from Course import *
 
 # -------------------------------------------
 
@@ -17,23 +22,106 @@ df = pd.read_csv('cmpt-courses-cleaned.csv')
 
 # Create the app and add extra styles
 app = dash.Dash(
+    __name__,
     external_stylesheets=[dbc.themes.BOOTSTRAP]
 )
 
-# Column 1 - Input
+
+class CollapseList:
+    def __init__(self):
+        self.list = []
+
+    def append(self, other):
+        self.list.append(other)
+
+    def getList(self):
+        return self.list  # list of id string of collapse groups
+
+
+collapseList = CollapseList()
+
+
+def makeCollapse(i):
+    # course_id = course.id.split(' ')
+
+    # collapseList.append(f"{course_id[0]}-{course_id[1]}-collapse-toggle")
+
+    return dbc.Card(
+        [
+            dbc.CardHeader(
+                html.H2(
+                    dbc.Button(
+                        f"Collapsible group #{i}",
+                        color="link",
+                        id=f"group-{i}-toggle",
+                    )
+                )
+            ),
+            dbc.Collapse(
+                dbc.CardBody(f"This is the content of group {i}..."),
+                id=f"collapse-{i}",
+            ),
+        ]
+    )
+
+cmpt103 = Course("CMPT 103", "Introduction to Computing II", 3,
+                 "This course continues the overview of computing science concepts that was started in CMPT 101. Topics include representation of compound data using abstraction, programming languages, and modularity; algorithms that use these data structures; and networks with the TCP/IP model and client/server architecture. Students continue with the syntax of a high-level programming language: functions, arrays, and user-defined data types.",
+                 ())
+
+'''
+Column 1 - Input
+'''
 input_col = dbc.Col(
     dbc.Container([
+
         html.H1("Inputs"),  # Title
-        dcc.Dropdown(
-            options=[
-                {'label': 'Edmonton', 'value': 'YEG'},
-                {'label': '', 'value': ''}
-            ],
-            value='YEG'
-        )
+
+        dbc.Row(dbc.Col([
+            html.B("Courses"),
+            dcc.Dropdown(
+                options=[
+                    {'label': 'Edmonton', 'value': 'YEG'},
+                    {'label': '', 'value': ''}
+                ],
+                value='YEG'
+            )
+        ])),
+
+        dbc.Row(dbc.Col([
+            makeCollapse(1),
+            makeCollapse(2),
+            makeCollapse(3),
+        ])),
+
+        dbc.Row(dbc.Col([
+            html.B("Mark selected course(s) as:"),
+            dcc.RadioItems(
+                options=[
+                    {'label': 'Completed', 'value': 'done'},
+                    {'label': 'Work In Progress', 'value': 'wip'},
+                    {'label': 'Planning to Take', 'value': 'planned'}
+                ],
+                value='done',
+                labelStyle={'display': 'block'}  # make new line option
+            ),
+            dbc.Button("Add to Planner", color="primary", id='add-to-planner', n_clicks=0),
+
+        ])),
+
+        dbc.Container([
+            dbc.Row(html.B("Change view:")),
+            dbc.Row([
+                dbc.Button("Default", color="primary", id='view-btn-default', n_clicks=0),
+                dbc.Button("Sunburst", color="primary", id='view-btn-sun', n_clicks=0),
+                html.Div(id='container-button-timestamp')
+            ]),
+        ]),
+
+        # dbc.Row(dbc.Col([])),
     ]), width=3
 )
 
+print(' '.join(map(str, collapseList.getList())))
 '''
 Column 2 - Data Tables
 '''
@@ -139,6 +227,47 @@ app.layout = html.Div(style={'backgroundColor': '#00000'},
                           ),
                       ],
                       )
+
+
+@app.callback(
+    # Output('container-button-timestamp', 'children'),
+
+    [Output(f"collapse-{i}", "is_open") for i in range(1, 4)],
+
+    # Input('view-btn-default', 'n_clicks'),
+    # Input('view-btn-sun', 'n_clicks'),
+
+    [Input(f"group-{i}-toggle", "n_clicks") for i in range(1, 4)],
+    [State(f"collapse-{i}", "is_open") for i in range(1, 4)],
+)
+# def displayClick(btn1, btn2):
+#     print(btn1)
+#     changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
+#     # print(changed_id)
+#     if 'view-btn-default' in changed_id:
+#         msg = 'Button 1 was most recently clicked'
+#         changed_id
+#     elif 'view-btn-sun' in changed_id:
+#         msg = 'Button 2 was most recently clicked'
+#     return html.Div(msg)
+
+
+def toggle_accordion(n1, n2, n3, is_open1, is_open2, is_open3):
+    ctx = dash.callback_context
+
+    if not ctx.triggered:
+        return False, False, False
+    else:
+        button_id = ctx.triggered[0]["prop_id"].split(".")[0]
+
+    if button_id == "group-1-toggle" and n1:
+        return not is_open1, False, False
+    elif button_id == "group-2-toggle" and n2:
+        return False, not is_open2, False
+    elif button_id == "group-3-toggle" and n3:
+        return False, False, not is_open3
+    return False, False, False
+
 
 # Run the app
 if __name__ == '__main__':
